908a85e3601db57ed6c8479317fc175995c178ab
Axel <axelleonr12@gmail.com>
2019-12-15 01:13:46 -0300

    Updates

---
+++ precache-manifest.e90e2435e0a9a641bcbb4ab09584c2a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d77393d20c9fbb2bb0258c289c96fb",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "413e6ecaed08cadff10a",
+    "url": "/Portfolio/static/css/main.6edf2a9f.chunk.css"
+  },
+  {
+    "revision": "c53f781fb8e1da1350ef",
+    "url": "/Portfolio/static/js/2.8a3222e8.chunk.js"
+  },
+  {
+    "revision": "413e6ecaed08cadff10a",
+    "url": "/Portfolio/static/js/main.8be4d862.chunk.js"
+  },
+  {
+    "revision": "c5287e694b1745deb535",
+    "url": "/Portfolio/static/js/runtime-main.c86b25dc.js"
+  },
+  {
+    "revision": "31913b931ea78bf4cbf5f094f8f37bf9",
+    "url": "/Portfolio/static/media/black-background16.31913b93.jpg"
+  },
+  {
+    "revision": "a4ddcfa46530944306ba7526424afffd",
+    "url": "/Portfolio/static/media/profilepic.a4ddcfa4.jpg"
+  },
+  {
+    "revision": "a7212ed4c02ca28a98c310a947c21998",
+    "url": "/Portfolio/static/media/react2.a7212ed4.png"
+  }
+]);
